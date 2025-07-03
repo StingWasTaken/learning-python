e0fdd81ec0531ca5bad0749303024a69ea1d8583
Alvaro Berruezo <Alvaro.Berruezo@sunwebgroup.com>
2025-07-03 10:48:00 +0200

    Fix sample and clean up

--- samples/sample-002.py
+++ samples/sample-002.py
@@ -4,7 +4,7 @@ number = input()
 print('You picked:', number)
 print('Pick another number:')
 number2 = input()
-print('You picked:', number2)2
+print('You picked:', number2)
 print('The sum of the two numbers is:', int(number) + int(number2))
 
 # Knowding the type of a variable
